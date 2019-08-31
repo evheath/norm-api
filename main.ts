ee6e3b44b42922acc05aa8a79ae285c51170cda5
evheath <elliot.v.heath@gmail.com>
2019-08-31 10:52:16 -0500

    calculating alt and azi

--- main.ts
+++ main.ts
@@ -1,2 +1,17 @@
-let x = 1;
-console.log(x + 3)
+console.clear()
+
+import * as SunCalc from 'suncalc'
+
+const bozemanLat = 45.6841
+const bozemanLong = -111.052
+
+
+// get position of the sun (azimuth and altitude) at today's sunrise
+var sunPosNow = SunCalc.getPosition(new Date(), bozemanLat, bozemanLong);
+
+var bozemanAzimuthInDegrees = sunPosNow.azimuth * 180 / Math.PI;
+console.log(`azimuth ${180 + bozemanAzimuthInDegrees}`)
+
+
+const bozemanAltitudeInDegrees = sunPosNow.altitude * 180 / Math.PI
+console.log(`altitude ${bozemanAltitudeInDegrees}`)
